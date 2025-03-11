92932d7d346fee545b0037fab4b3a69625e54e0d
dz940 <123370406+dz940@users.noreply.github.com>
2025-03-11 12:32:17 +0000

    Add files via upload

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,192 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   app_icon_small_png;
+    const int            app_icon_small_pngSize = 2522;
+
+    extern const char*   app_icon_png;
+    const int            app_icon_pngSize = 8371;
+
+    extern const char*   cloudDown_png;
+    const int            cloudDown_pngSize = 2131;
+
+    extern const char*   cloudOff_png;
+    const int            cloudOff_pngSize = 2177;
+
+    extern const char*   cloudOn_png;
+    const int            cloudOn_pngSize = 2286;
+
+    extern const char*   knob1_png;
+    const int            knob1_pngSize = 2443;
+
+    extern const char*   knob2_png;
+    const int            knob2_pngSize = 2221;
+
+    extern const char*   knob3_png;
+    const int            knob3_pngSize = 2588;
+
+    extern const char*   knob4_png;
+    const int            knob4_pngSize = 3312;
+
+    extern const char*   knob5_png;
+    const int            knob5_pngSize = 3566;
+
+    extern const char*   load_png;
+    const int            load_pngSize = 1185;
+
+    extern const char*   overlay_clouds_dark_png;
+    const int            overlay_clouds_dark_pngSize = 15173;
+
+    extern const char*   overlay_clouds_light_png;
+    const int            overlay_clouds_light_pngSize = 13952;
+
+    extern const char*   overlay_clouds_partial_png;
+    const int            overlay_clouds_partial_pngSize = 14482;
+
+    extern const char*   overlay_precipitation_rain_png;
+    const int            overlay_precipitation_rain_pngSize = 44939;
+
+    extern const char*   overlay_precipitation_snow_png;
+    const int            overlay_precipitation_snow_pngSize = 14474;
+
+    extern const char*   overlay_stage_rainy_png;
+    const int            overlay_stage_rainy_pngSize = 13766;
+
+    extern const char*   overlay_stage_snowy_png;
+    const int            overlay_stage_snowy_pngSize = 16121;
+
+    extern const char*   overlay_stage_standard_png;
+    const int            overlay_stage_standard_pngSize = 9648;
+
+    extern const char*   overlay_sun_png;
+    const int            overlay_sun_pngSize = 10080;
+
+    extern const char*   overlay_wind_left_1_png;
+    const int            overlay_wind_left_1_pngSize = 14274;
+
+    extern const char*   overlay_wind_left_2_png;
+    const int            overlay_wind_left_2_pngSize = 20418;
+
+    extern const char*   overlay_wind_left_3_png;
+    const int            overlay_wind_left_3_pngSize = 32430;
+
+    extern const char*   overlay_wind_left_4_png;
+    const int            overlay_wind_left_4_pngSize = 42686;
+
+    extern const char*   overlay_wind_left_5_png;
+    const int            overlay_wind_left_5_pngSize = 62469;
+
+    extern const char*   overlay_wind_right_1_png;
+    const int            overlay_wind_right_1_pngSize = 14202;
+
+    extern const char*   overlay_wind_right_2_png;
+    const int            overlay_wind_right_2_pngSize = 20198;
+
+    extern const char*   overlay_wind_right_3_png;
+    const int            overlay_wind_right_3_pngSize = 32613;
+
+    extern const char*   overlay_wind_right_4_png;
+    const int            overlay_wind_right_4_pngSize = 42767;
+
+    extern const char*   overlay_wind_right_5_png;
+    const int            overlay_wind_right_5_pngSize = 62124;
+
+    extern const char*   pause_png;
+    const int            pause_pngSize = 1117;
+
+    extern const char*   play_png;
+    const int            play_pngSize = 1180;
+
+    extern const char*   rainDown_png;
+    const int            rainDown_pngSize = 1998;
+
+    extern const char*   rainOff_png;
+    const int            rainOff_pngSize = 2072;
+
+    extern const char*   rainOn_png;
+    const int            rainOn_pngSize = 2199;
+
+    extern const char*   rainyScene_png;
+    const int            rainyScene_pngSize = 36634;
+
+    extern const char*   sliderThumb_png;
+    const int            sliderThumb_pngSize = 1992;
+
+    extern const char*   snowDown_png;
+    const int            snowDown_pngSize = 2571;
+
+    extern const char*   snowOff_png;
+    const int            snowOff_pngSize = 2816;
+
+    extern const char*   snowOn_png;
+    const int            snowOn_pngSize = 2907;
+
+    extern const char*   snowyScene_png;
+    const int            snowyScene_pngSize = 22242;
+
+    extern const char*   stage_png;
+    const int            stage_pngSize = 9648;
+
+    extern const char*   stickman_png;
+    const int            stickman_pngSize = 1948;
+
+    extern const char*   stickmanRainy_png;
+    const int            stickmanRainy_pngSize = 3648;
+
+    extern const char*   stickmanSnowy_png;
+    const int            stickmanSnowy_pngSize = 2902;
+
+    extern const char*   stop_png;
+    const int            stop_pngSize = 1099;
+
+    extern const char*   sunDown_png;
+    const int            sunDown_pngSize = 2449;
+
+    extern const char*   sunnyScene_png;
+    const int            sunnyScene_pngSize = 30170;
+
+    extern const char*   sunOff_png;
+    const int            sunOff_pngSize = 2585;
+
+    extern const char*   sunOn_png;
+    const int            sunOn_pngSize = 2707;
+
+    extern const char*   title_png;
+    const int            title_pngSize = 26811;
+
+    extern const char*   windDown_png;
+    const int            windDown_pngSize = 2552;
+
+    extern const char*   windOff_png;
+    const int            windOff_pngSize = 2861;
+
+    extern const char*   windOn_png;
+    const int            windOn_pngSize = 3007;
+
+    extern const char*   windyScene_png;
+    const int            windyScene_pngSize = 60937;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 55;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
